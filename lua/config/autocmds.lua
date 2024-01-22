fbe89e8c58e6f35704fb078a7fd34bfb89144a4a
Tomasz Kaczmarzyk <tspamm3r@gmail.com>
2024-01-22 23:12:02 +0100

    initial commit of nvim config

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,31 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- Disable autoformat for lua files
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "toml" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+-- Something somewhere is setting tw=99 for rust
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "rust" },
+  callback = function()
+    vim.o.tw = 79
+  end,
+})
+
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = {
+      start = { args.line1, 0 },
+      ["end"] = { args.line2, end_line:len() },
+    }
+  end
+  require("conform").format({ async = true, lsp_fallback = true, range = range })
+end, { range = true })
---
+++ lua/plugins/core.lua
@@ -0,0 +1,317 @@
+-- change trouble config
+local util = require("lspconfig.util")
+return {
+
+  {
+    "stevearc/dressing.nvim",
+    opts = {
+      select = {
+        telescope = nil,
+      },
+    },
+  },
+  {
+    "gbprod/yanky.nvim",
+    dependencies = { { "kkharji/sqlite.lua", enabled = not jit.os:find("Windows") } },
+    keys = {
+      { "<leader>p", false },
+      {
+        "<leader>P",
+        function()
+          require("telescope").extensions.yank_history.yank_history({})
+        end,
+        desc = "Open Yank History",
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    opts = {},
+    keys = {
+      { "<leader>tt", false },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Run File",
+      },
+    },
+  },
+  -- interative rename
+  { "smjonas/inc-rename.nvim", config = true },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pylsp = {
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  ignore = { "W391", "E501" },
+                  maxLineLength = 120,
+                },
+              },
+            },
+          },
+        },
+        -- rust_analyzer = {
+        --   keys = {
+        --     { "<leader>cR", false },
+        --     { "<leader>cr", "<cmd>RustCodeAction<cr>", desc = "Code Action (Rust)" },
+        --   },
+        -- },
+        rust_analyzer = {},
+        -- postgres_lsp = {
+        --   default_config = {
+        --     name = "postgres_lsp",
+        --     cmd = { "/Users/adn/.asdf/installs/rust/1.72/bin/postgres_lsp" },
+        --     filetypes = { "sql" },
+        --     single_file_support = true,
+        --     root_dir = util.root_pattern("root-file.txt"),
+        --   },
+        -- },
+      },
+      setup = {
+        rust_analyzer = function()
+          return true
+        end,
+      },
+    },
+    init = function()
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+
+      -- Change rename from `<leader>cr` to `<leader>cR`
+      keys[#keys + 1] = { "<leader>cr", false }
+
+      if require("lazyvim.util").has("inc-rename.nvim") then
+        keys[#keys + 1] = {
+          "<leader>cn",
+          function()
+            local inc_rename = require("inc_rename")
+            return ":" .. inc_rename.config.cmd_name .. " " .. vim.fn.expand("<cword>")
+          end,
+          expr = true,
+          desc = "Rename",
+          has = "rename",
+        }
+      else
+        keys[#keys + 1] = { "<leader>cn", vim.lsp.buf.rename, desc = "Rename", has = "rename" }
+      end
+    end,
+  },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "akinsho/bufferline.nvim", opts = { options = { mode = "tabs" } } },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local icons = require("lazyvim.config").icons
+      local Util = require("lazyvim.util")
+
+      opts.inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+        },
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {},
+      }
+
+      opts.sections = vim.tbl_extend("force", opts.sections, {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+        lualine_c = {
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+        },
+        lualine_x = {
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = Util.ui.fg("Constant"),
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function () return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = Util.ui.fg("Debug"),
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+          },
+        },
+        lualine_y = {},
+        lualine_z = {
+          { "progress", separator = "", padding = { left = 0, right = 0 } },
+          { "location", padding = { left = 0, right = 0 } },
+        },
+      })
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        mappings = {
+          ["o"] = "open",
+        },
+      },
+    },
+  },
+  {
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        char = {
+          keys = {
+            "f",
+            "F",
+            "t",
+            "T",
+            ";",
+          },
+        },
+      },
+    },
+  },
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+    keys = {
+      { ",,", false },
+      -- disable the keymap to grep files
+      { "<leader>/", false },
+      -- disable telescope git mappings
+      { "<leader>gc", false },
+      { "<leader>gs", false },
+      -- find files
+      { "<leader>fg", "<cmd>Telescope git_status<cr>", desc = "Git status files" },
+      {
+        "<leader>ch",
+        '<cmd>lua require("dash.providers.telescope").dash({ bang = false, initial_text = "" })<cr>',
+        desc = "Dash",
+      },
+    },
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.sorting.priority_weight = 10
+
+      opts.mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.confirm({ select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<S-CR>"] = cmp.mapping.confirm({
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = true,
+        }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        ["<C-c>"] = cmp.mapping.abort(),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
+            cmp.select_next_item()
+          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+          -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    init = function()
+      -- Needed for trees-context floating win border
+      -- https://github.com/neovim/neovim/issues/17996#issuecomment-1186208986
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#021316" })
+    end,
+  },
+  { "echasnovski/mini.pairs"},
+  {
+    "mfussenegger/nvim-lint",
+    opts = function(_, opts)
+      opts.linters_by_ft = {
+        markdown = { "vale" },
+      }
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      -- formatters = {
+      --   sql_formatter = {
+      --     command = "pg_format",
+      --     args = "-f 1 -u 1 -U 1 -s 2 --no-space-function --no-extra-line --nogrouping -k",
+      --   },
+      -- },
+      formatters_by_ft = {
+        -- sql = { "sql_formatter" },
+        -- pgsql = { "sql_formatter" },
+        -- python = { "black" },
+        c = { "clang_format" },
+      },
+    },
+  },
+}
